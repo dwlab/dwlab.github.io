8f710f09585d5b2ed50b879d0eb39ad295b670f7
Tingyan <46310179+tingyanlin@users.noreply.github.com>
2024-01-23 17:08:40 +0800

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,12 +1,34 @@
 var members = [
     {
-        imgSrc: "https://dwlab.github.io/images/林亭延.jpg",
+        imgSrc: "https://dwlab.github.io/images/students/林亭延.jpg",
         name: "林亭延",
         position: "112 博士生",
         description: "",
         socialLinks: [
             { icon: "icon-twitter", url: "mailto:tingyanlin.77@gmail.com" },
             { icon: "icon-facebook", url: "https://www.facebook.com/tingyan.lin.319" },
+            { icon: "icon-github", url: "#" }
+        ]
+    },
+    {
+        imgSrc: "https://dwlab.github.io/images/students/person2.jpg",
+        name: "蔡嘉安",
+        position: "111 博士生",
+        description: "",
+        socialLinks: [
+            { icon: "icon-twitter", url: "#" },
+            { icon: "icon-facebook", url: "#" },
+            { icon: "icon-github", url: "#" }
+        ]
+    },
+    {
+        imgSrc: "https://dwlab.github.io/images/students/黃祈勝.jpg",
+        name: "黃祈勝",
+        position: "100 博士生",
+        description: "在分散式雲端平台上對不同巨量天文應用之資料區域性適用策略研究",
+        socialLinks: [
+            { icon: "icon-globe2", url: "https://l.facebook.com/l.php?u=https%3A%2F%2Forcid.org%2F0000-0001-5763-1143&amp;h=ATO-cdRwpMh82YkSF7JCl3M6Gdf0L983gOlMuqEnKBh1JcUvTwrm7n4YhFM-LBGrANxapc1seUnW7b7iMxpVb1rN8BWUP2E0N5me1xStA9ck-hyXxDVonqU8x2c" },
+            { icon: "icon-facebook", url: "#" },
             { icon: "icon-github", url: "#" }
         ]
     },
